fcae5670a47d194c1dfa6203c5287d9eca5883d2
Miguel-ectil <ectilmiguel@gmail.com>
2025-09-18 15:44:59 -0300

    feat: [ADD] Adicionando agendamento de tarefa a cada certo tempo ela execuat a tarefa

--- celery_worker.py
+++ celery_worker.py
@@ -1,11 +1,22 @@
 from app import create_app
 from app.extensions import celery
 from app.config import Config
+from celery.schedules import crontab
+from datetime import timedelta
 
 def make_celery(app):
     celery.conf.broker_url = Config.CELERY_BROKER_URL
     celery.conf.result_backend = Config.CELERY_RESULT_BACKEND
     celery.conf.imports = ['app.tasks.monitor_tasks']
+
+    # Configuração do Celery Beat para executar a tarefa a cada 5 minutos
+    celery.conf.beat_schedule = {
+        'verificar-sites-cada-5-minutos': {
+            'task': 'app.tasks.monitor_tasks.verificar_sites',
+            'schedule': timedelta(seconds=30),  # a cada 5 minutos
+        },
+    }
+    celery.conf.timezone = 'UTC'  # Ajuste para seu fuso horário, se quiser
 
     class ContextTask(celery.Task):
         def __call__(self, *args, **kwargs):
@@ -17,7 +28,6 @@ def make_celery(app):
 
 flask_app = create_app()
 celery = make_celery(flask_app)
-
 flask_app.extensions['celery'] = celery
 
 if __name__ == '__main__':
